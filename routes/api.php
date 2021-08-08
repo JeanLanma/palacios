65f59767ab213f147a6a18c1c19963c84ea6e8ab
Jean Langarica <jeanmacario048@gmail.com>
2021-08-07 22:21:50 -0500

    Preproduccion

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/marca/{id}', [MarcasController::class, 'show'])->name('api.marcas.show');
-Route::get('/marca', [MarcasController::class, 'showAll'])->name('api.marcas.showAll');
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
