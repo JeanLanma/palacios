2dc74daf57a61bf277f80bee85ab65cb63937c86
Jean Langarica <jeanmacario048@gmail.com>
2021-08-08 03:28:37 -0500

    agregada ruta para el storage

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\MarcasController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,8 @@ Route::get('/marcas/{id}/edit', [MarcasController::class, 'edit'])->name('admin.
 Route::get('/marca/{id}', [MarcasController::class, 'show'])->name('admin.marcas.show');
 Route::post('/marcas', [MarcasController::class, 'store'])->name('admin.marcas.store');
 Route::post('/marcas/{id}', [MarcasController::class, 'update'])->name('admin.marcas.update');
+
+Route::get('/generate/storage/link', function(){
+    Artisan::call('storage:link');
+    return response('Ok', 201);
+});
